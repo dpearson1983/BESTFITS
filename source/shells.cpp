cc2868c836ea68e7a1e282dcd445410ebd4216cc
David Pearson <dpearson@localhost.localdomain>
2018-07-31 13:32:21 -0500

    Peak bispectrum estimate with N_tri norm without FFT norm

--- source/shells.cpp
+++ source/shells.cpp
@@ -187,7 +187,7 @@ void get_bispectrum(std::vector<double> &ks, std::vector<double> &P, vec3<double
                     
                     double B_est = shell_prod(shell_1, shell_2, shell_3, N);
                     double SN = (P[i] + P[j] + P[k])*gal_bk_nbw.y + gal_bk_nbw.x - alpha*alpha*ran_bk_nbw.x;
-//                     B_est *= V_f/V_ijk;
+                    B_est *= 1.0/V_ijk;
 //                     B_est -= SN;
 //                     B_est /= gal_bk_nbw.z;
                     B.push_back(B_est);
